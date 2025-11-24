a8034730daa4c8f8d71da81e35b144ea6387a894
Kench Ralfhael Bejec <bejec.kenchralfhael@gmail.com>
2025-11-25 00:17:26 +0800

    feat: Add tsx for script execution and view-leads command; refactor: Update font in app/layout.tsx from Montserrat to Poppins.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google"; // Changed from Montserrat
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -39,6 +40,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
