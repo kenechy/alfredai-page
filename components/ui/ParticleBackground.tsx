2fdf539e29118891b072d03456eb71de5f94955d
Kench Ralfhael Bejec <bejec.kenchralfhael@gmail.com>
2025-11-24 23:50:42 +0800

    fix: build error in ParticleBackground component

--- components/ui/ParticleBackground.tsx
+++ components/ui/ParticleBackground.tsx
@@ -15,7 +15,7 @@ interface Particle {
 export function ParticleBackground() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const particlesRef = useRef<Particle[]>([]);
-  const animationFrameRef = useRef<number>();
+  const animationFrameRef = useRef<number>(0);
 
   useEffect(() => {
     const canvas = canvasRef.current;
